2ebae8347da6afb63fc85eb5c6e1877d14fdac47
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2025-04-03 11:40:20 +0400

    add header content

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const body = document.querySelector("body");
 const colorToggleBtnText = document.querySelector(".btn-text-color");
+const headerTitle = document.querySelector('.header-title');
 const colorToggle = document.querySelector(".color-toggle-btn");
+
 colorToggle.addEventListener("click", () => {
   body.classList.toggle("light-mode");
   colorToggleBtnText.classList.toggle("text-color-grey");
+  headerTitle.classList.toggle('text-color-black')
 });
